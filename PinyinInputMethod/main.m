0a35f399640a89df2c6a925d3e5e779b0aa4e98c
SimonLiu <sliu009@gmail.com>
2026-08-18 03:46:17 +0800

    fix: IMKServer connection name mismatch, clean up Info.plist input mode config

--- PinyinInputMethod/main.m
+++ PinyinInputMethod/main.m
@@ -8,18 +8,16 @@
 #import <Cocoa/Cocoa.h>
 #import <InputMethodKit/InputMethodKit.h>
 
+// 输入法连接名称，需与 Info.plist 中 InputMethodConnectionName 保持一致
+static NSString * const kConnectionName = @"com.ximeng.inputmethod.connection";
 // 输入法 Bundle Identifier，需与 Info.plist 中保持一致
 static NSString * const kInputMethodBundleID = @"com.ximeng.inputmethod";
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        // 获取输入法 Bundle
-        NSBundle *bundle = [NSBundle bundleForClass:[NSObject class]];
-        
         // 连接输入法服务器
-        // IMKServer 是 Input Method Kit 的核心类，负责与系统输入法框架通信
-        IMKServer *server = [[IMKServer alloc] initWithName:kInputMethodBundleID
-                                          bundleIdentifier:[[NSBundle mainBundle] bundleIdentifier]];
+        IMKServer *server = [[IMKServer alloc] initWithName:kConnectionName
+                                          bundleIdentifier:kInputMethodBundleID];
         
         if (!server) {
             NSLog(@"[PinyinInputMethod] 错误：无法创建 IMKServer");
